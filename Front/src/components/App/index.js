e101bc57d9e4dd38822e2f2b2f9eb8a7c991ef6c
holydaimon <rihal.anthony@gmail.com>
2021-07-19 18:39:25 +0200

    ajout useEffect pour récupération du json et poursuite implémentation de la vue

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Icon } from 'semantic-ui-react';
+import Loader from '../Loader';
 
 import './styles.css';
 
 const Debugger = () => {
   const [search, setSearch] = useState('');
   const [result, setResult] = useState('');
+  const [airport, setAirport] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    };
+    axios.get('https://www.cloudping.cloud/cloudfront-edge-locations.json', config)
+      .then((response) => {
+        console.log('response de location', response);
+        const airportArray = response.nodes;
+        setAirport(airportArray);
+        console.log('airportArray', airportArray);
+      });
+  }, []);
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    setIsLoading(true);
     const config = {
       url: { search },
     };
@@ -17,6 +36,9 @@ const Debugger = () => {
       .then((response) => {
         const apiResponse = response.data;
         setResult(apiResponse);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
 
@@ -53,28 +75,36 @@ const Debugger = () => {
           <div className="square" />
           <h2>HISTORY</h2>
         </div>
-        <table>
-          <thead>
-            <tr>
-              <th className="centered">Date</th>
-              <th className="noCentered">URL</th>
-              <th className="centered">Status</th>
-              <th className="noCentered">Flags</th>
-              <th className="centered">Cloudfront status</th>
-              <th className="noCentered">Cloudfront pop</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="rowResult">
-              <td className="centered">la date</td>
-              <td className="noCentered">{search}</td>
-              <td className="centered">{result.statusCode}</td>
-              <td className="noCentered">{result.fstrzFlags}</td>
-              <td className="centered">{result.cloudfrontStatus}</td>
-              <td className="noCentered">{result.cloudfrontPOP}</td>
-            </tr>
-          </tbody>
-        </table>
+        {result !== ''
+          ? (
+            <table>
+              <thead>
+                <tr>
+                  <th className="centered">Date</th>
+                  <th className="noCentered">URL</th>
+                  <th className="centered">Status</th>
+                  <th className="noCentered">Flags</th>
+                  <th className="centered">Cloudfront status</th>
+                  <th className="noCentered">Cloudfront pop</th>
+                </tr>
+              </thead>
+              <tbody>
+                {!isLoading
+                  ? (
+                    <tr className="rowResult">
+                      <td className="centered">la date</td>
+                      <td className="noCentered">{search}</td>
+                      <td className="centered">{result.statusCode === 200 ? <Icon color="green" name="cloud" /> : <Icon textAlign="center" color="red" name="cloud" />}</td>
+                      <td className="noCentered">{result.fstrzFlags}</td>
+                      <td className="centered"><span className="CFStatus">{result.cloudfrontStatus}</span></td>
+                      <td className="noCentered">{result.cloudfrontPOP}</td>
+                    </tr>
+                  )
+                  : <tr />}
+              </tbody>
+            </table>
+          ) : ''}
+        {isLoading ? <Loader /> : ''}
       </div>
     </div>
   );
---
+++ Front/src/components/Loader/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Loader as SemanticLoader } from 'semantic-ui-react';
+
+const Loader = () => (
+  <SemanticLoader size="medium" active />
+
+);
+
+export default Loader;
